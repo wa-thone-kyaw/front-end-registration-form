2d3d3bc2c86886471e1456bbff4f1a08f461ebb6
MyatMonThant <myatmonthant.org@gmail.com>
2023-07-30 17:30:54 +0630

    completed user

--- src/component/Registerationform/Firstyear.jsx
+++ src/component/Registerationform/Firstyear.jsx
@@ -28,6 +28,51 @@ export const Firstyear = () => {
   const [phone_no, setPhoneNo] = useState("");
   const [email, setEmail] = useState("");
 
+  const [selectedValue, setSelectedValue] = useState("");
+  const [selectedValue2, setSelectedValue2] = useState("");
+  const [selectedValue3, setSelectedValue3] = useState("");
+  const [selectedValue4, setSelectedValue4] = useState("");
+  const [selectedValue5, setSelectedValue5] = useState("");
+
+  const inputRef = useRef(null);
+  const [photo, setImage] = useState("");
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+    setImage(file);
+  };
+
+  const options = [
+    "",
+    "Civil",
+    "Electronics",
+    "Electrical Power",
+    "Mechanical",
+    "Information Technology",
+  ];
+
+  const onOptionChangeHandler1 = (event) => {
+    setSelectedValue(event.target.value);
+    console.log("Selected major:", event.target.value);
+  };
+  const onOptionChangeHandler2 = (event) => {
+    setSelectedValue2(event.target.value);
+    console.log("Selected major:", event.target.value);
+  };
+  const onOptionChangeHandler3 = (event) => {
+    setSelectedValue3(event.target.value);
+    console.log("Selected major:", event.target.value);
+  };
+  const onOptionChangeHandler4 = (event) => {
+    setSelectedValue4(event.target.value);
+    console.log("Selected major:", event.target.value);
+  };
+  const onOptionChangeHandler5 = (event) => {
+    setSelectedValue5(event.target.value);
+    console.log("Selected major:", event.target.value);
+  };
+
   const handleMyanNameChange = (e) => {
     setMyanname(e.target.value);
   };
@@ -90,11 +135,82 @@ export const Firstyear = () => {
     setEmail(e.target.value);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("myanname", myanname);
+    formData.append("engname", engname);
+    formData.append("nrc", nrc);
+    formData.append("birthDay", birthDay);
+    formData.append("nation", nation);
+    formData.append("seatno", seatno);
+    formData.append("score", score);
+    formData.append("department", department);
+    formData.append("myanfathername", myanfathername);
+    formData.append("engfathername", engfathername);
+    formData.append("fathernrc", fathernrc);
+    formData.append("fathernation", fathernation);
+    formData.append("fatherjob", fatherjob);
+    formData.append("mothername", mothername);
+    formData.append("mothernrc", mothernrc);
+    formData.append("mothernation", mothernation);
+    formData.append("motherjob", motherjob);
+    formData.append("address", address);
+    formData.append("phone_no", phone_no);
+    formData.append("email", email);
+    formData.append("photo", photo);
+    formData.append("selectedValue", selectedValue);
+    formData.append("selectedValue2", selectedValue2);
+    formData.append("selectedValue3", selectedValue3);
+    formData.append("selectedValue4", selectedValue4);
+    formData.append("selectedValue5", selectedValue5);
+
+    /* setMyanname("");
+    setEngname("");
+    setNrc("");
+    setBirthDay("");
+    setNation("");
+    setSeatNo("");
+    setDepartment("");
+    setScore("");
+    setMyanFatherName("");
+    setEngFatherName("");
+    setFatherNrc("");
+    setFatherNation("");
+    setFatherJob("");
+    setMotherName("");
+    setMotherNrc("");
+    setMotherNation("");
+    setMotherJob("");
+    setAddress("");
+    setPhoneNo("");
+    setEmail("");
+    setPhoto(null); */
+
+    /* alert(
+      "You registered successfully! Then click Next button and read the university rules. "
+    ); */
+    try {
+      const response = await Axios.post(
+        "http://127.0.0.1:8000/student_registration/first_year",
+        formData
+      );
+      const newItemId = response.data.id;
+      alert(
+        `You registered successfully! Then click Next button and read the university rules and fill this ID ${newItemId} in the Fill ID form.`
+      );
+      console.log("Data sent successfully:", response.data);
+    } catch (error) {
+      console.error("Error sending data:", error);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div class="background">
         <div class="uploadphoto">
-          {/* <div className="image-upload-container">
+          <div className="image-upload-container">
             <div className="box-decoration">
               <div
                 onClick={() => inputRef.current.click()}
@@ -127,7 +243,7 @@ export const Firstyear = () => {
                 />
               </div>
             </div>
-          </div> */}
+          </div>
         </div>
 
         <div class="header">
@@ -383,12 +499,97 @@ export const Firstyear = () => {
               required
             />
           </div>
+          <div class="courses">
+            <br />
+            <h2>ဖွင့်လှစ်မည့်သင်တန်းများ</h2>
+            <br />
 
-          {/* <div class="submit">
+            <div>
+              <ol>
+                <li>Civil(မြို့ပြသင်တန်း)</li>
+                <li>Electronics(အီလက်ထရောနစ်)</li>
+                <li>Electrical Power(လျှပ်စစ်စွမ်းအား)</li>
+                <li>Mechanical(စက်မှုစွမ်းအား)</li>
+                <li>Information Technology(သုတနည်းပညာ)</li>
+              </ol>
+            </div>
+            <br />
+            <br />
+            <h2>
+              တက်‌ရောက်လိုသောသင်တန်း(ကွက်လက်အားလုံးပြည့်စုံစွာဖြည့်စွက်ပါရန်)
+            </h2>
+            <br />
+            <div>
+              <ul>
+                <li>
+                  ပထမဦးစားပေး{" "}
+                  <select
+                    value={selectedValue}
+                    onChange={onOptionChangeHandler1}
+                  >
+                    {options.map((option, index) => {
+                      return <option key={index}>{option}</option>;
+                    })}
+                  </select>
+                </li>
+                <br />
+                <li>
+                  ဒုတိယဦးစားပေး{" "}
+                  <select
+                    value={selectedValue2}
+                    onChange={onOptionChangeHandler2}
+                  >
+                    {options.map((option, index) => {
+                      return <option key={index}>{option}</option>;
+                    })}
+                  </select>
+                </li>
+                <br />
+                <li>
+                  တတိယဦးစားပေး{" "}
+                  <select
+                    value={selectedValue3}
+                    onChange={onOptionChangeHandler3}
+                  >
+                    {options.map((option, index) => {
+                      return <option key={index}>{option}</option>;
+                    })}
+                  </select>
+                </li>
+                <br />
+                <li>
+                  စတုတ္ထဦးစားပေး{" "}
+                  <select
+                    value={selectedValue4}
+                    onChange={onOptionChangeHandler4}
+                  >
+                    {options.map((option, index) => {
+                      return <option key={index}>{option}</option>;
+                    })}
+                  </select>
+                </li>
+                <br />
+                <li>
+                  ပဥ္စမဦးစားပေး{" "}
+                  <select
+                    value={selectedValue5}
+                    onChange={onOptionChangeHandler5}
+                  >
+                    {options.map((option, index) => {
+                      return <option key={index}>{option}</option>;
+                    })}
+                  </select>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <br />
+
+          <div class="submit">
             <button type="submit" required>
               Submit
             </button>
-          </div> */}
+          </div>
 
           <div className=" back">
             <Link to="/chooseyear">
@@ -396,36 +597,11 @@ export const Firstyear = () => {
             </Link>
           </div>
 
-          <div className="next">
-            <Link
-              to={{
-                pathname: "/choosemajor",
-                state: {
-                  myanname,
-                  engname,
-                  nrc,
-                  birthDay,
-                  nation,
-                  seatno,
-                  score,
-                  department,
-                  myanfathername,
-                  engfathername,
-                  fathernrc,
-                  fathernation,
-                  fatherjob,
-                  mothername,
-                  mothernrc,
-                  mothernation,
-                  motherjob,
-                  address,
-                  phone_no,
-                  email,
-                },
-              }}
-            >
+          <div class="next">
+            {" "}
+            <Link to="/agreeform">
               <button>Next</button>
-            </Link>
+            </Link>{" "}
           </div>
         </div>
       </div>
